21af119857e879b6897d19cf00ebea33a896a7a3
Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
2023-12-22 02:31:28 -0500

    agregada clase user para el trabajo con usuarios

---
+++ class/user.py
@@ -0,0 +1,9 @@
+class User():
+    """
+    Class for users
+    """
+    def __init__(self, id, name, password, vlan):
+        self.id = id
+        self.name = name
+        self.password = password
+        self.vlan = vlan
