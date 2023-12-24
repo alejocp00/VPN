137ceede2c2b56069c384fdd8479996884f11378
Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
2023-12-23 22:24:38 -0500

    agregadas operaciones crud para userIprange

---
+++ database/usersIprangeDb.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+def insert_userIprange(userId, ipRangeId):
+    conn = sqlite3.connect('vpn.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO users_iprange VALUES (?, ?)",
+              userId, ipRangeId)
+    conn.commit()
+    conn.close()
+
+def select_iprange_by_user(userId):
+    conn = sqlite3.connect('vpn.db')
+    c = conn.cursor()
+    c.execute("SELECT iprangeId FROM users_iprange WHERE userId=?", (userId,))
+    ipranges = c.fetchall()
+    conn.close()
+    return ipranges
+
+def delete_vlan(userId, ipRangeId):
+    conn = sqlite3.connect('vpn.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM users_iprange WHERE userId=? and iprangeId=?", (userId, ipRangeId)
+              )
+    conn.commit()
+    conn.close()
