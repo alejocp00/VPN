46197d93ca58eeecbf3d6a70c4a9078d5405f3f6
Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
2023-12-23 20:53:19 -0500

    agregada tabla para entidad ips

--- database/database.py
+++ database/database.py
@@ -6,22 +6,33 @@ c = conn.cursor()
 c.execute("""CREATE TABLE IF NOT EXISTS users(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL
+        password TEXT NOT NULL,
         vlanId INTEGER NOT NULL,
-        FOREIGN KEY (vlanId) REFERENCES vlans (id)
+        ipId INTEGER NOT NULL,
+        FOREIGN KEY (vlanId) REFERENCES vlans (id),
+        FOREIGN KEY (ipId) REFERENCES ips (id)
 ) """)
           
 c.execute("""CREATE TABLE IF NOT EXISTS vlans(
         id INTEGER PRIMARY KEY,
-        inicialIpRange VARCHAR,
-        finalIpRange VARCHAR
+        ipNetwork VARCHAR, 
+        mask VARCHAR
 ) """)
+          
+          # ToDo: arreglar atributos de la vlan 
           
 c.execute("""CREATE TABLE IF NOT EXISTS iprange(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         inicialIp VARCHAR NOT NULL,
         finalIp VARCHAR NOT NULL
 )""")
+          
+c.execute("""CREATE TABLE IF NOT EXISTS ips(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        ip VARCHAR NOT NULL UNIQUE,
+        vlanId INTEGER NOT NULL,
+        FOREIGN KEY (vlanId) REFERENCES vlans (id)
+) """) 
           
 c.execute("""CREATE TABLE IF NOT EXISTS users_iprange(
         userId INTEGER NOT NULL,
