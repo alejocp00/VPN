d0ec216032f274d7e1fd7b4dd3b69db908528eba
Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
2023-12-23 21:37:37 -0500

    agregada operaciones crud para ips

---
+++ database/ipDb.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+def insert_ip(ip):
+    conn = sqlite3.connect('vpn.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO ips VALUES (?,)",
+              (ip,))
+    conn.commit()
+    conn.close()
+
+def select_ip(id):
+    conn = sqlite3.connect('vpn.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM ips WHERE id=?", (id,))
+    ip = c.fetchone()
+    conn.close()
+    return ip
+
+def delete_ip(id):
+    conn = sqlite3.connect('vpn.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM ips WHERE id=?", (id,)
+              )
+    conn.commit()
+    conn.close()
+
+def select_all_ips():
+    conn = sqlite3.connect('vpn.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM ips")
+    ips = c.fetchall()
+    conn.close
+    return ips
