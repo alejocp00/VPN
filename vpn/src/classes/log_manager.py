61f6e962515ac39bb7fde03620999da895aaac05
AlejandroCamachoPerez <alejandro.camacho@estudiantes.matcom.uh.cu>
2023-12-27 04:12:49 -0500

    Created the log manager class

--- vpn/src/classes/log_manager.py
+++ vpn/src/classes/log_manager.py
@@ -1,13 +1,31 @@
 class LogManager:
-    def __init__(self, log_file_path):
+    def __init__(self):
         self.__logs = []
+        self.__new_log = False
+        self.__logs_to_show = 10
 
     def add_log(self, log):
+        self.__new_log = True
         self.__logs.append(log)
 
     def get_all_log(self):
-        return self.__logs
+        return "\n".join(self.__logs)
 
-    def print_logs(self):
-        for log in self.__logs:
-            print(log)
+    def get_logs(self):
+        """Return the amount of logs indicated by __logs_to_show
+
+        Returns:
+            string: logs formatted to be printed
+        """
+
+        # Update flag
+        self.__new_log = False
+
+        # Check if are enough logs
+        if len(self.__logs) < self.__logs_to_show:
+            return "\n".join(self.__logs)
+
+        return "\n".join(self.__logs[-self.__logs_to_show])
+
+    def new_logs(self):
+        return self.__new_log
