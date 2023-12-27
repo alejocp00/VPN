ab9d8b25e33c1b21fcd0719fdf95e112f89f3a1a
AlejandroCamachoPerez <alejandro.camacho@estudiantes.matcom.uh.cu>
2023-12-27 04:42:08 -0500

    Added close method to be used

--- common/protocols/my_socket.py
+++ common/protocols/my_socket.py
@@ -1,7 +1,14 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
+import socket
 
 
 class MySocket(metaclass=ABCMeta):
     def __init__(self) -> None:
+        # fix: need to be raw
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # Todo: Implement
         pass
+
+    def close(self):
+        # Todo:implement close socket function
+        self._socket.close()
