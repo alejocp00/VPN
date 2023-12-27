a1b88a54df1ccb544be5f25c7cffc750224ea109
AlejandroCamachoPerez <alejandro.camacho@estudiantes.matcom.uh.cu>
2023-12-27 01:53:47 -0500

    "Add VPN server IP address and port to common_variables.py"

--- common/common_variables.py
+++ common/common_variables.py
@@ -1,6 +1,4 @@
 from protocols.utils import *
 
-VPN_SERVER_ADDRESS = "localhost"
+VPN_SERVER_IP = "localhost"
 VPN_SERVER_PORT = 8080
-vpn_status = VPNStatus.IDLE
-vpn_protocol = VPNProtocol.UNKNOWN
