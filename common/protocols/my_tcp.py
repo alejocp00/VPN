21549ead874d52a47a25f8c1029a6e9150b6e5e5
AlejandroCamachoPerez <alejandro.camacho@estudiantes.matcom.uh.cu>
2023-12-31 11:34:31 -0500

    Refactor MySocket class to support different protocols

--- common/protocols/my_tcp.py
+++ common/protocols/my_tcp.py
@@ -1,33 +1,31 @@
-
+from audioop import add
 from common.protocols.my_socket import MySocket
 import socket
 
 
-class MyTCP(MySocket):
-
+class MyTCP:
     def __init__(self, sock=None):
         # Todo: Add the code to initialize the class
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #self.socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+        # self.socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
     def bind(self, ip, port):
         self.socket.bind((ip, port))
 
-    def listen(self) :
+    def listen(self):
         self.socket.listen()
 
     def accept(self):
         return self.socket.accept()
-    
+
     def send(self, data):
         self.socket.send(data)
 
     def recv(self, buffer_size):
         return self.socket.recv(buffer_size)
-    
+
     def close(self):
         self.socket.close()
 
-    def connect(self, ip, port):
-        self.socket.connect((ip, port))
-
+    def connect(self, address: tuple[int, int]):
+        self.socket.connect(address)
