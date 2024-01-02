50b116ebc0c596b085f9bdaf01dcc79f9b5341b8
Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
2024-01-02 15:18:31 -0500

    agregada funcion para calcular capacidad de vlan al crear una vlan

--- vpn/src/classes/vlan.py
+++ vpn/src/classes/vlan.py
@@ -1,9 +1,23 @@
+import ipaddress
+
+
 class Vlan():
     """
     Class for vlans
     """
-    def __init__(self, id, network, mask, hostNumber):
+    def __init__(self, id, network, mask):
         self.id = id
         self.network = network
         self.mask = mask
-        self.hostNumber = hostNumber
+        self.hostNumber = self.__get_max_hosts()
+
+
+    def __get_max_hosts(self):
+        "Calculates the maximum number of hosts that a vlan supports"
+        # Create a network object using address and mask
+        network = ipaddress.IPv4Network(self.network + '/' + self.mask, strict=False)
+
+        # Calculate the maximum number of computers in the VLAN
+        max_hosts = network.num_addresses - 2  # Subtract network and broadcast address
+
+        return max_hosts
