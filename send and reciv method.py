41da916797a9d88884748d1a7d5b0b1f18b9b885
ArturoMetalhead <perezcabreracarlosarturo@gmail.com>
2024-01-02 23:06:53 -0500

    arreglando errores

--- send and reciv method.py
+++ send and reciv method.py
@@ -4,20 +4,36 @@ socket = MyUDP()
 
 def send_file(file,dest_addr):
     file = open(file,"r")
-    data = file.read()
+    data="**{file.name}**}"######
+    data += file.read()
     file.close()
     socket.send(data.encode(),dest_addr)
 
 def send_message(message,dest_addr):
+    message="*msg*"+message
     socket.send(message.encode(),dest_addr)
 
 
-def recv_file(file_name,buffer_size):
+def recv(buffer_size):
     data = socket.recv(buffer_size)
-    file = open(file_name,"w")
-    file.write(data)
-    file.close()
+
+    if(data[0:5].decode()=="*msg*"):
+        return data[5:].decode()
+    else:
+        file_name = data[2:data.find("}**")]########
+        data=data[data.find("}**")+3:]##########
+
+        file = open(file_name,"w")
+        file.write(data)
+        file.close()
+
+
+
+
+
+    
 
 def recv_message(buffer_size):
     data = socket.recv(buffer_size)
     return data.decode()
+
